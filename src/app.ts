1217edd8ace0b367fc181aaf54d5f75216265c2d
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-04 16:45:04 -0300

    Adiciona typescript e linter ao projeto
    adicionar os pacotes de typescript e eslint ao projeto,
    e seus respectivos arquivos de configuração.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-function helloWorld() {
-  return console.log("Hello World")
-}
+import e from 'express'
+
+const app = e()
+const port = 5000
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!")
+})
+
+app.listen(port, () => {
+  console.log(`Hello, express application listening on port ${port}`)
+})
+
