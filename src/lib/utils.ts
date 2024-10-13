2d284f4dbfb3652099f4b73f51f5e735174fe969
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-13 00:31:32 -0300

    Adiciona a função utilitária de validação de CPF

---
+++ src/lib/utils.ts
@@ -0,0 +1,33 @@
+export function validaCpf(cpf: string) {
+  const cpfFormatted = cpf.replace(/\D/g, "")
+
+  if(cpfFormatted.length !== 11) return false
+  if(/^(\d)\1*$/.test(cpfFormatted)) return false
+
+  let soma = 0
+  for(let i = 0; i < 9; i++) {
+    const digit = parseInt(cpfFormatted[i])
+    soma += digit * (10 - i)
+  }
+
+  let resto = soma % 11
+
+  const primeiroDigito = resto < 2 ? 0 : 11 - resto
+
+  if(parseInt(cpfFormatted[9]) !== primeiroDigito) return false
+
+  soma = 0
+  for(let i = 0; i < 10; i++) {
+    const digit = parseInt(cpfFormatted[i])
+    soma += digit * (11 - i)
+  }
+
+  resto = soma % 11
+
+  const segundoDigito = resto < 2 ? 0 : 11 - resto
+
+  if(parseInt(cpfFormatted[10]) !== segundoDigito) return false
+  else return true
+}
+
+export function validaEmail() {}
