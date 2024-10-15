894e64ccb106b4dc1fee35a991d0063995fbe817
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-15 12:42:48 -0300

    Atualiza a nomenclatura dos casos de uso.

--- src/useCases/gerenciarDespesa.ts
+++ src/useCases/gerenciarDespesa.ts
@@ -4,8 +4,8 @@ import { Despesa } from "../entities/Despesa.ts";
 export interface GerenciarDespesaI {
   cadastrar(despesa: Despesa): Promise<void>
   buscar(carteiraId: number): Promise<Despesa[]>
+  atualizar(despesa: Despesa): Promise<void>
   excluir(uuid: string): Promise<void>
-  salvar(despesa: Despesa): Promise<void>
 }
 
 export class GerenciarDespesa implements GerenciarDespesaI {
@@ -19,11 +19,12 @@ export class GerenciarDespesa implements GerenciarDespesaI {
     return this.despesaRepo.load(carteiraId)
   }
 
+  async atualizar(despesa: Despesa): Promise<void> {
+    this.despesaRepo.save(despesa)
+  }
+  
   async excluir(uuid: string): Promise<void> {
     this.despesaRepo.delete(uuid)
   }
 
-  async salvar(despesa: Despesa): Promise<void> {
-    this.despesaRepo.save(despesa)
-  }
 }
--- src/useCases/gerenciarRenda.ts
+++ src/useCases/gerenciarRenda.ts
@@ -3,8 +3,8 @@ import { Renda } from "../entities/Renda.ts";
 export interface GerenciarRendaI {
   cadastrar(renda: Renda): Promise<void>
   buscar(carteiraId: number): Promise<Renda[]>
+  atualizar(renda: Renda): Promise<void>
   excluir(uuid: string): Promise<void>
-  salvar(renda: Renda): Promise<void>
 }
 export class GerenciarRenda implements GerenciarRendaI {
   constructor(private rendaRepo: RendaRepo) {}
@@ -17,11 +17,11 @@ export class GerenciarRenda implements GerenciarRendaI {
     return this.rendaRepo.load(carteira_id)
   }
 
-  async excluir(uuid: string) {
-    this.rendaRepo.delete(uuid)
+  async atualizar(renda: Renda) {
+    this.rendaRepo.save(renda)
   }
 
-  async salvar(renda: Renda) {
-    this.rendaRepo.save(renda)
+  async excluir(uuid: string) {
+    this.rendaRepo.delete(uuid)
   }
 }
