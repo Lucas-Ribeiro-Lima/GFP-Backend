342d2f9360ecbaf865a891e604104dcbf06bb8bd
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-16 12:33:27 -0300

    Adiciona uma nova função middleware para verificação de propriedades presentes no corpo da requisição.

--- src/lib/middlewares/validacoes.ts
+++ src/lib/middlewares/validacoes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export function requestBodyValido(req: Request, res: Response, next: NextFunction): Response | void {
   if (!req.body || Object.keys(req.body).length === 0) {
@@ -12,4 +12,21 @@ export function requestParamsValido(req: Request, res: Response, next: NextFunct
     return res.status(400).json({ error: "Requisição sem parâmetros" });
   }
   next();
+}
+
+export function requestBodyFieldPresentes<T>(objeto: T,req: Request, res: Response, next: NextFunction): Response | void {
+  const bodyPropriedades = Object.getOwnPropertyNames(req.body)
+  const objetoPropriedades = Object.getOwnPropertyNames(objeto)
+  const propriedadesFaltantes: Array<string> = []
+
+  objetoPropriedades.forEach((propriedade) => {
+    if(!bodyPropriedades.includes(propriedade) && propriedade !== "constructor") propriedadesFaltantes.push(propriedade)
+  })
+
+  if(propriedadesFaltantes.length) return res.status(400).json({
+    error: "Propriedades faltantes no corpo da requisição",
+    propriedades: propriedadesFaltantes
+  })
+
+  next()
 }
--- src/lib/test/utils.spec.ts
+++ src/lib/test/utils.spec.ts
@@ -1,7 +1,5 @@
-import { NextFunction, Request, Response } from 'express'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { cpfValido, emailValido } from '../utils.ts'
-import { requestBodyValido, requestParamsValido } from '../middlewares/validacoes.ts'
 
 
 describe("Testes para as funções utilitárias", () => {  
@@ -34,47 +32,3 @@ describe("Testes para as funções utilitárias", () => {
   })
 })
 
-describe("Testes para as funções middleware para roteamente", () => {
-  const req = {
-    params: {},
-    body: {}
-  } as unknown as Request 
-
-  const res = {
-    status: vi.fn().mockReturnThis(),
-    json: vi.fn().mockReturnThis()
-  } as unknown as Response
-
-  const next = vi.fn() as unknown as NextFunction
-
-  it("deve retornar status 400 caso a requisição não possua paramêtros", () => {
-    requestParamsValido(req, res, next)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.json).toHaveBeenCalledWith({ error: "Requisição sem parâmetros" })
-  })
-
-  it("deve chamar a função next caso a função possua paramêtros", () => {
-    req.params.id = '0'
-
-    requestParamsValido(req, res, next)
-
-    expect(next).toHaveBeenCalled()
-  })
-
-  it("deve retornar status 400 caso a requisição não possua corpo", () => {
-    requestBodyValido(req, res, next)
-
-    expect(res.status).toBeCalledWith(400)
-    expect(res.json).toBeCalledWith({error: "Requisição sem corpo"})
-  })
-
-  it("deve chamar a função next caso a função possua corpo", () => {
-    req.body = {
-      name: "john doe"
-    }
-
-    requestBodyValido(req, res, next)
-
-    expect(next).toHaveBeenCalled()
-  })
-})
