7c44b22db3e751bf0ab1f89374ff3fc91656e05e
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-17 16:13:36 -0300

    Atualiza a tipagem das validações e remove os retornos

--- src/lib/middlewares/validacoes.ts
+++ src/lib/middlewares/validacoes.ts
@@ -1,20 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 
-export function requestBodyValido(req: Request, res: Response, next: NextFunction): Response | void {
+export async function requestBodyValido(req: Request, res: Response, next: NextFunction): Promise<void> {
   if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).json({ error: "Requisição sem corpo" });
+    res.status(400).json({ error: "Requisição sem corpo" });
   }
   next();
 }
 
-export function requestParamsValido(req: Request, res: Response, next: NextFunction): Response | void {
+export async function requestParamsValido(req: Request, res: Response, next: NextFunction): Promise<void> {
   if (!req.params || Object.keys(req.params).length === 0) {
-    return res.status(400).json({ error: "Requisição sem parâmetros" });
+    res.status(400).json({ error: "Requisição sem parâmetros" });
   }
   next();
 }
 
-export function requestBodyFieldPresentes<T>(objeto: T,req: Request, res: Response, next: NextFunction): Response | void {
+export async function requestQueryValido(req: Request, res: Response, next: NextFunction): Promise<void> {
+  if (!req.query || Object.keys(req.query).length === 0) {
+    res.status(400).json({ error: "Requisição sem parâmetros" });
+  }
+  next();
+}
+
+export async function requestBodyFieldPresentes<T>(objeto: T,req: Request, res: Response, next: NextFunction): Promise<void> {
   const bodyPropriedades = Object.getOwnPropertyNames(req.body)
   const objetoPropriedades = Object.getOwnPropertyNames(objeto)
   const propriedadesFaltantes: Array<string> = []
@@ -23,7 +30,7 @@ export function requestBodyFieldPresentes<T>(objeto: T,req: Request, res: Respon
     if(!bodyPropriedades.includes(propriedade) && propriedade !== "constructor") propriedadesFaltantes.push(propriedade)
   })
 
-  if(propriedadesFaltantes.length) return res.status(400).json({
+  if(propriedadesFaltantes.length) res.status(400).json({
     error: "Propriedades faltantes no corpo da requisição",
     propriedades: propriedadesFaltantes
   })
