3367a3cf2c9864180238b6ddc9eb4ba753c840c9
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-17 17:05:09 -0300

    Ajusta as validações para interromper o fluxo de execução da rota e evitar retornos duplicados.

--- src/lib/middlewares/validacoes.ts
+++ src/lib/middlewares/validacoes.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 export async function requestBodyValido(req: Request, res: Response, next: NextFunction): Promise<void> {
   if (!req.body || Object.keys(req.body).length === 0) {
     res.status(400).json({ error: "Requisição sem corpo" });
+    return
   }
   next();
 }
@@ -10,6 +11,7 @@ export async function requestBodyValido(req: Request, res: Response, next: NextF
 export async function requestParamsValido(req: Request, res: Response, next: NextFunction): Promise<void> {
   if (!req.params || Object.keys(req.params).length === 0) {
     res.status(400).json({ error: "Requisição sem parâmetros" });
+    return
   }
   next();
 }
@@ -17,6 +19,7 @@ export async function requestParamsValido(req: Request, res: Response, next: Nex
 export async function requestQueryValido(req: Request, res: Response, next: NextFunction): Promise<void> {
   if (!req.query || Object.keys(req.query).length === 0) {
     res.status(400).json({ error: "Requisição sem parâmetros" });
+    return
   }
   next();
 }
@@ -30,10 +33,13 @@ export async function requestBodyFieldPresentes<T>(objeto: T,req: Request, res: 
     if(!bodyPropriedades.includes(propriedade) && propriedade !== "constructor") propriedadesFaltantes.push(propriedade)
   })
 
-  if(propriedadesFaltantes.length) res.status(400).json({
+  if(propriedadesFaltantes.length) {
+    res.status(400).json({
     error: "Propriedades faltantes no corpo da requisição",
     propriedades: propriedadesFaltantes
-  })
+   })
+   return
+  }
 
   next()
 }
