73b34ecd49fd5a26e4cc984ffdaf1e16067a3109
Lucaslive974 <lucasribeirolima974@gmail.com>
2024-10-17 23:05:56 -0300

    Feat: Adiciona novas configurações para o Swagger e authorização básica baseada no hostname.

--- src/routes/routeSwagger.ts
+++ src/routes/routeSwagger.ts
@@ -1,6 +1,7 @@
-import { Router } from "express";
+import { Router, Response, Request, NextFunction } from "express";
 import { serve, setup } from 'swagger-ui-express'
 import swaggerJsDocs from 'swagger-jsdoc'
+import { envs } from "../configs/env.ts";
 
 export const routeSwagger = Router()
 
@@ -8,10 +9,36 @@ const optionsJsDocs = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Gestor de Finanças Pessoais',
+      title: 'Swagger Gestor de Finanças Pessoais',
       version: '1.0.0',
+      description: "<strong>API para aplicação GFP<strong/>",
+      servers: [
+      {
+       "url": "http://localhost:5000",
+       "description": "Servidor de desenvolvimento"
+      }
+      ],
+      tags: [
+        {
+          "name": "Contas",
+          "description": "Serviços de gerenciamento de contas"
+        },
+        {
+          "name": "Carteiras",
+          "description": "Serviços de gerenciamento de carteiras"
+        },
+        {
+          "name": "Rendas",
+          "description": "Serviços de gerenciamento de rendas"
+        },
+        {
+          "name": "Despesas",
+          "description": "Serviços de gerenciamento de despesas"
+        },
+      ]
     },
   },
+  components: {},
   apis: ['./src/**/*.ts'], // files containing annotations as above
 };
 
@@ -22,4 +49,13 @@ const optionsUi = {
 }
 
 routeSwagger.use('/', serve)
-routeSwagger.get('/', setup(openApiSpecification, optionsUi))
+routeSwagger.get('/', authorizateDocs, setup(openApiSpecification, optionsUi))
+
+
+function authorizateDocs(req: Request, res: Response, next: NextFunction) {
+  if(req.hostname !== envs.EXPRESS_HOST) {
+    res.status(403).json({message: "Acesso proibido"})
+    return
+  }
+  next()
+}
