5520a395b2ca39f80b6cdf766b8a1fcde4c6bbdb
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-18 13:26:23 -0300

    feature: Finalização das rotas de conta.

--- src/routes/routeConta.ts
+++ src/routes/routeConta.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import { controllerConta } from '../configs/controllers.ts'
-import { requestBodyValido } from '../lib/middlewares/validacoes.ts'
+import { requestBodyValido, requiredtBodyProps } from '../lib/middlewares/validacoesHttp.ts'
 
 export const routeConta = Router()
+
+const requiredPropEmail = requiredtBodyProps(["email"])
+const requiredPropConta = requiredtBodyProps([
+  "id", "nome", "email", "cpf", "provider", "configs"
+])
 
 routeConta.get("/", async (req, res) => {
   res.json({
@@ -30,7 +35,7 @@ routeConta.get("/", async (req, res) => {
  *      404:
  *         description: Returna null se não exister uma conta com o e-mail.
  */
-routeConta.post("/buscar", requestBodyValido, async (req, res, next) => {
+routeConta.post("/buscar", requestBodyValido, requiredPropEmail, async (req, res, next) => {
   try {
     await controllerConta.handleHttpGet(req,res)
   } catch (error) {
@@ -38,4 +43,106 @@ routeConta.post("/buscar", requestBodyValido, async (req, res, next) => {
   }
 })
 
+/**
+ * @openapi
+ * /conta/criar:
+ *  post:
+ *    tags:
+ *      - Contas
+ *    summary: Criar uma conta
+ *    security:
+ *      - openIdConnect: []
+ *    description: Cria uma conta com os dados enviados
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        aplication/json:
+ *          example:
+ *            conta:
+ *               nome: johndoe
+ *               email: johndoe@doe.uk
+ *               cpf: 123456789-09
+ *               provider: Google
+ *    responses:
+ *      201:
+ *        description: Conta criada com sucesso
+ *      400:
+ *        description: Json inválido
+ *      500:
+ *        description: Erro interno    
+ */
+routeConta.post("/criar", requestBodyValido, async (req, res, next) => {
+  try {
+    await controllerConta.handleHttpPost(req, res)
+  } catch (error) {
+    next(error)
+  } 
+})
 
+/**
+ * @openapi
+ * /conta/atualizar:
+ *  patch:
+ *    tags:
+ *      - Contas
+ *    security:
+ *      - openIdConnect: []
+ *    summary: Atualiza uma conta
+ *    description: Atualiza uma conta baseada nos dados enviados
+ *    requestBody:
+ *      required: true
+ *      content:
+ *         aplication/json:
+ *          example:
+ *            id: 0
+ *            nome: Doe John
+ *            email: johndoe@doe.uk
+ *            cpf: 12345678909
+ *            provider: Microsoft
+ *            configs:
+ *              tema: dark            
+ *              displayName: Does
+ *              customWpp: myCustomWpp
+ *    responses:
+ *      200: 
+ *        description: Conta atualizada com sucesso
+ *      500: 
+ *        description: Erro ao atualizar uma conta
+ */
+routeConta.patch("/atualizar", requestBodyValido, requiredPropConta, async (req, res, next) => {
+  try {
+    await controllerConta.handleHttpPatch(req, res)
+  } catch (error) {
+    next(error)
+  }
+})
+
+/**
+ * @openapi
+ * /conta/excluir:
+ *  delete:
+ *    tags:
+ *      - Contas
+ *    security:
+ *      - openIdConnect: []
+ *    summary: Deleta uma conta
+ *    description: Deleta uma conta baseada nos email enviados
+ *    requestBody:
+ *      required: true
+ *      content:
+ *         aplication/json:
+ *          example:
+ *            email: johndoe@doe.uk
+ *    responses:
+ *      200: 
+ *        description: Conta deletada com sucesso
+ *      500: 
+ *        description: Erro ao deletar uma conta
+ */
+routeConta.delete("/excluir", requestBodyValido, async (req, res, next) => {
+  try {
+    await controllerConta.handleHttpDelete(req, res)
+  } catch (error) {
+    next(error)
+  }
+})
