1503ff6ad91b7ec5ec5a5164dfc6850df0dec412
lucas.lima <lucasribeirolima974@gmail.com>
2024-10-18 13:25:49 -0300

    Feature: Adiciona ma nova costumização de console para a inicialização.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ const port = envs.EXPRESS_PORT
 
 app.listen(port, () => {
   console.log(`\n\x1b[1m\x1b[92mHello, \x1b[33mGFP application\x1b[0m listening on:\x1b[3m\x1b[36m http://${host}:${port}/\x1b[0m\n`)
-  console.log(`\x1b[1m\x1b[41m API-Docs \x1b[0mcan be found on \x1b[3m\x1b[95mhttp://${host}:${port}/api-docs\x1b[0m`)
+  console.log(`\x1b[1m\x1b[41m API-Docs \x1b[0mcan be found on \x1b[3m\x1b[36mhttp://${host}:${port}/api-docs\x1b[0m`)
 })
 
