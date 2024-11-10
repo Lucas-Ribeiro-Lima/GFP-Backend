a6ce31e733cdd9316513c95a2abda5e53d1a57f4
lucas.lima <lucasribeirolima974@gmail.com>
2024-11-09 22:52:50 -0300

    feat: Adiciona função de encerramento para desligamento servidor de forma graciosa
    Este commit adiciona um novo arquivo `terminate.ts`. Essa função é responsável por desligar o servidor graciosamente, fechando a instância do servidor e saindo do processo. Ele também inclui um parâmetro opcional `Dump` para controlar se um dump de núcleo deve ser gerado e um parâmetro `termination para especificar o tempo em milissegundos para esperar antes de sair com força do processo.
    
    Em `main.ts`, a função terminate é importada e usada para lidar com vários eventos de saída, como exceções não capturadas, rejeições não tratadas, SIGTERM e sinais SIGINT. Isso garante que o servidor seja encerrado corretamente e que os recursos sejam liberados quando esses eventos ocorrerem.
    
    Esse commit melhora a confiabilidade e a estabilidade do aplicativo do servidor.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { app } from './app.ts'
 import { envs } from './configs/env.ts'
+import { terminate } from './lib/terminate.ts'
 
 const host = envs.EXPRESS_HOST
 const port = envs.EXPRESS_PORT
 
-app.listen(port, () => {
+
+const httpserver = app.listen(port, () => {
   console.log(`\n\x1b[1m\x1b[92mHello, \x1b[33mGFP application\x1b[0m listening on:\x1b[3m\x1b[36m http://${host}:${port}/\x1b[0m\n`)
   console.log(`\x1b[1m\x1b[41m API-Docs \x1b[0mcan be found on \x1b[3m\x1b[36mhttp://${host}:${port}/api-docs\x1b[0m`)
 })
 
+const exitHandler = terminate(httpserver, { coreDump: false, timeout: 1500 })
+
+process.on("uncaughtException", () => exitHandler(1));
+process.on("unhandledRejection", () => exitHandler(1));
+process.on("SIGTERM", () => exitHandler(0));
+process.on("SIGINT", () => exitHandler(0));
