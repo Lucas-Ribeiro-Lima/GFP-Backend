84c4e17d1d83bc4e8e26a8f075a7e5d1c00f4436
lucas.lima <lucasribeirolima974@gmail.com>
2024-11-11 09:33:06 -0300

    Refatoração: Ajuste da resposta em caso de requisições não autenticadas para apenas status 401, sem o redirecionamento.

--- src/lib/middlewares/authentication.ts
+++ src/lib/middlewares/authentication.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
   if(!req.isAuthenticated() || !req.user) {
-    res.status(401).redirect("/auth/google")
+    res.status(401).send()
     return
   }
   next()
